90a8d6076fb9e4f95f3c2393f8711d86f06b2f3d
root <blbl@qq.com>
2022-03-29 22:23:46 +0800

    hot-fix commit1

--- src/main/java/gittext.java
+++ src/main/java/gittext.java
@@ -1,8 +1,9 @@
 public class gittext {
     public static void main(String[] args){
         System.out.println("hello world");
-        System.out.println("hello git");
-        System.out.println("hello git2");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hot-fix");
 
     }
 }
