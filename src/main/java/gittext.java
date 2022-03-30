6d268d899d4863ac18cd352123efebff9bda0428
12138-aaa <67490921+12138-aaa@users.noreply.github.com>
2022-03-30 09:07:13 +0800

    Update gittext.java

--- src/main/java/gittext.java
+++ src/main/java/gittext.java
@@ -5,6 +5,7 @@ public class gittext {
         System.out.println("hello world");
         System.out.println("master");
         System.out.println("hot-fix");
+        SYstem.out.print("远程修改");
 
     }
 }
